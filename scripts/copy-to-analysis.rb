5ff90701ec16ba1a98007d3ebd8c429d198f727d
ruse-traveler <derek.murphy.anderson@protonmail.com>
2024-12-17 18:34:13 -0500

    Update analysis path

--- scripts/copy-to-analysis.rb
+++ scripts/copy-to-analysis.rb
@@ -13,7 +13,7 @@ require 'fileutils'
 
 # top directory to copy from/to
 copy_from = "/sphenix/user/danderson/eec/SLambdaJetHunter"
-copy_to   = "/sphenix/user/danderson/sphenix/analysis/AndersonAnalysisModules/ColdQcdCorrelatorAnalysis/SLambdaJetHunter"
+copy_to   = "/sphenix/user/danderson/sphenix/analysis/EnergyCorrelatorsJets/ColdQCDENC/SLambdaJetHunter"
 
 # what files to copy
 to_copy = [
